b6136a9fe1c16f08069f0af6ef17f4c12c65794e
pham-thien <pham.thien@outlook.com>
2017-05-26 14:13:39 -0400

    finished actions

--- client/src/actions.js
+++ client/src/actions.js
@@ -1,20 +1,31 @@
 export const GET_CHEESES = 'GET_CHEESES';
 export const getCheeses = () => ({
-    type: GET_CHEESES,
+    type: GET_CHEESES
+});
+
+export const GET_CHEESES_SUCCESS = 'GET_CHEESES_SUCCESS';
+export const getCheesesSuccess = (data) => ({
+    type: GET_CHEESES_SUCCESS,
     data
 });
-export const REQUEST_CHEESES = 'REQUEST_CHEESES';
-export const requestCheeses = () => ({
-    type: REQUEST_CHEESES
-})
 
-export const fetchCheeses = () => ({
-    return dispatch => {
-        dispatch(requestCheeses())
-        fetch('http:localhost:8080/api/cheeses')
-        .then(response => response.json())
-        .then(cheeses => console.log(cheeses))
-        .catch(err => console.log(err))
-    }
-        
-})
+export const GET_CHEESES_ERROR = 'GET_CHEESES_ERROR';
+export const getCheesesError = (error) => ({
+    type: GET_CHEESES_ERROR,
+    error
+});
+
+export const fetchCheeses = () => dispatch => {
+    dispatch(getCheeses());
+    fetch('http:localhost:8080/api/cheeses').then(res => {
+        if(!res.ok){
+            return Promise.reject(res.statusText);
+        }
+        return res.json();
+    }).then(data => {
+        dispatch(getCheesesSuccess(data));
+    }).catch(error => {
+        dispatch(getCheesesError(error));
+    });   
+};
+
--- client/src/components/cheese-list.js
+++ client/src/components/cheese-list.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import App from './app';
+// import App from '../app';
 import {connect} from 'react-redux';
-import {GET_CHEESES, REQUEST_CHEESES,fetchCheeses} from './actions';
+import {GET_CHEESES, REQUEST_CHEESES,fetchCheeses} from '../actions';
 
-export function CheeseList(props){
-    componentDidMount() {
-        props.dispatch(fetchCheeses());
-    }
+export const CheeseList = (props) => {
+    // const cheeseList = props.cheeses.map((cheese, index) => {
+    //     <li key={index}>{cheese.type}</li>
+    // })
+    // componentDidMount() {
+    //     props.dispatch(fetchCheeses());
+    // }
          return(
             <ul>
-                {this.props.cheeses.map((cheese, index) => 
+                {props.cheeses.map((cheese, index) => 
                     <li key={index}>{cheese.type}</li>
                     )}
             </ul>
